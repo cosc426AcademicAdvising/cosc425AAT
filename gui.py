53f9f5ed90c4293676498cd2d5405bf39ba2df82
chiefdondjeu <florentdondjeu@yahoo.com>
2021-02-09 23:31:45 -0500

    added filler frame for testing

--- gui.py
+++ gui.py
@@ -7,19 +7,33 @@ std_year = "Junior"
 std_grad = "2030"
 
 root = Tk()
+root.title("Academic Advising Tool")
 
-lframe = Frame(root, width=500, height=400)
+# frame for menu
+menu_frame = Frame(root,width=1000, height=25, bg='red')
+menu_frame.pack(side=TOP)
+
+# top frame
+tframe = Frame(root, width=1000, height=50, bg='white')
+tframe.pack(side=TOP)
+
+# left frame
+lframe = Frame(root, width=500, height=400, bg='green')
 lframe.pack(side=LEFT)
 
-frame = Frame(root, width=500, height=400)
-frame.pack(side=RIGHT)
+# right frame
+rframe = Frame(root, width=500, height=400)
+rframe.pack(side=RIGHT)
 
-student_info_frame = LabelFrame(frame, text="Student Information")
+# info frame inside right frame
+student_info_frame = LabelFrame(rframe, text="Student Information")
 student_info_frame.place(width=425, height=90)
 
 var = StringVar()
 label = Label(student_info_frame, textvariable=var, justify=LEFT, padx=20)
-var.set(" "*8 + "Name : " + std_name + '\n' +
+# concept output
+var.set(
+        " "*8 + "Name : " + std_name + '\n' +
         "Student ID : " + std_id + "\t"*3 + "Grad. Yr : " + std_grad + '\n' +
         " "*10 + "Year : " + std_year
         )
