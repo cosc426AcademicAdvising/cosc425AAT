5b980f6d7fae8b2dabdb8e9ca1f35a15da0c5da4
chiefdondjeu <florentdondjeu@yahoo.com>
2021-02-17 03:37:39 -0500

    update

---
+++ Florent/Controller.py
@@ -0,0 +1,33 @@
+from View import View
+from Model import Model
+import tkinter as tk
+from pubsub import pub      # pip install PyPubSub
+
+
+class Controller:
+    def __init__(self, master):
+        self.master = master
+        self.model = Model()
+        self.view = View(master)
+        self.view.setup()
+
+        # receive messages
+        pub.subscribe(self.openSchedule, "request student info")    #(action, msg key)
+        pub.subscribe(self.setVar,"student info sent")
+
+    def openSchedule(self):
+        print("student info request received")
+        self.model.openSchedule()
+
+    def setVar(self, arg1):
+        print("receive student info from controller")
+        self.view.setVar(arg1)
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    # root.geometry("%sx%s" % (1000, 600))
+    root.geometry("{0}x{1}+0+0".format(root.winfo_screenwidth(), root.winfo_screenheight()))  # fills whole window
+    root.title("Academic Advising Tool")
+
+    app = Controller(root)
+    root.mainloop()
--- Florent/View.py
+++ Florent/View.py
@@ -1,5 +1,5 @@
-from tkinter import *
 import tkinter as tk
+from pubsub import pub      # pip install PyPubSub
 
 def donothing():
     print("Something happened...")
@@ -8,7 +8,7 @@ def donothing():
 class View:
     def __init__(self, master):
         self.container = master
-        self.var = StringVar()
+        self.var = tk.StringVar()
 
     def setup(self):    # run first
         # methods to setup user interface
@@ -16,41 +16,37 @@ class View:
         self.setup_menuBar()
         self.setup_layout()
 
-    # temporary
-    # open file browser and accept json
-    def openFile(self):
-        print("file browser")
+    def openSchedule(self):
+        print("request student info")
+        pub.sendMessage("request student info")     # msg key
 
-    # testing function
-    # goal: sent var to Controller so it can be set
-    def getVar(self):
+    def setVar(self, data):
         self.var.set(
-            " " * 8 + "Name : " + "AAA" + '\n' +
-            "Student ID : " + "000" + "\t" * 3 + "Grad. Yr : " + "000" + '\n' +
-            " " * 10 + "Year : " + "000"
+            "Name: " + data[0] + "\n" +
+            "  ID: " + data[1] + "\n" +
+            "Year: " + data[2]
         )
 
     def create_widgets(self):
         # frames
-        self.leftFrame = Frame(self.container)
-        self.topFrame = Frame(self.container)
-        self.bottomFrame = Frame(self.container, bg='white')
+        self.leftFrame = tk.Frame(self.container)
+        self.topFrame = tk.Frame(self.container)
+        self.bottomFrame = tk.Frame(self.container, bg='white')
         # student information in top frame
-        self.studentInfoFrame = LabelFrame(self.topFrame)
-        self.studentInfoLabel = Label(self.studentInfoFrame, textvariable=self.var, fg='black', bg='white', justify=LEFT)
-        self.getVar() # functions for desired output
+        self.studentInfoFrame = tk.Frame(self.topFrame)
+        self.studentInfoLabel = tk.Label(self.studentInfoFrame, textvariable=self.var, fg='black', bg='white', justify=tk.LEFT)
 
     def setup_menuBar(self):
-        self.menuBar = Menu(self.container)
+        self.menuBar = tk.Menu(self.container)
         self.container.config(menu=self.menuBar)
         # SCHEDULE MENU
-        self.schedule = Menu(self.menuBar)
+        self.schedule = tk.Menu(self.menuBar)
         self.menuBar.add_cascade(label='Schedule', menu=self.schedule)
         # dropdown
         self.schedule.add_command(label='New...', command=donothing)
-        self.schedule.add_command(label='Open...', command=self.openFile)
+        self.schedule.add_command(label='Open...', command=self.openSchedule)
 
-        self.recent = Menu(self.schedule)
+        self.recent = tk.Menu(self.schedule)
         self.schedule.add_cascade(label="Open recent...", menu=self.recent)
         self.recent.add_separator()
         self.recent.add_command(label='Clear', command=donothing)
@@ -62,7 +58,7 @@ class View:
         self.schedule.add_command(label='Export', command=donothing)
         self.schedule.add_command(label='Print', command=donothing)
         # MAJOR MENU
-        self.major = Menu(self.menuBar)
+        self.major = tk.Menu(self.menuBar)
         self.menuBar.add_cascade(label='Major', menu=self.major)
         # drop down
 
@@ -73,14 +69,4 @@ class View:
         self.bottomFrame.place(relwidth=0.75, relheight=0.8, relx=0.25, rely=0.2)
 
         self.studentInfoFrame.place(relwidth=0.4, relheight=0.7, relx=0.2, rely=0.1)
-        self.studentInfoLabel.pack(fill=BOTH, expand=True)
-
-if __name__ == "__main__":
-    root = tk.Tk()
-    # root.geometry("%sx%s" % (1000, 600))
-    root.geometry("{0}x{1}+0+0".format(root.winfo_screenwidth(), root.winfo_screenheight()))  # fills whole window
-    root.title("Academic Advising Tool")
-
-    view = View(root)
-    view.setup()
-    root.mainloop()
+        self.studentInfoLabel.pack(fill=tk.BOTH, expand=True)
