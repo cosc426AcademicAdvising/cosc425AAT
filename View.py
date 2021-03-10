97652df0b7fe8c4a56766d871b9fd256135438d8
Devin287 <dts5398@hotmail.com>
2021-03-10 02:52:33 -0500

    Positioning Backup course table

--- View.py
+++ View.py
@@ -5,6 +5,7 @@ from pubsub import pub  # pip install PyPubSub
 import tkinter.font as TkFont
 from PIL import ImageTk, Image  # pip install pillow
 
+
 # import functionss as funct
 
 
@@ -98,17 +99,26 @@ class View:
 
     def PlanningWorksheet_layout(self):
         # outer most blank frames left & right
-        ''''
-        if self.mainwin.winfo_screenwidth() == 1920:
-            blank1 = Frame(self.rightFrame, width=self.mainwin.winfo_screenwidth()/10).grid(column=0, row=0, rowspan=30, sticky=(N,E,S,W))
-            blank2 = Frame(self.rightFrame, width=self.mainwin.winfo_screenwidth()/10).grid(column=5, row=0, rowspan=30, sticky=(N,E,S,W))
-        elif self.mainwin.winfo_screenwidth() == 1600:
+        width = self.mainwin.winfo_screenwidth()
+        height = self.mainwin.winfo_screenheight()
+        AspectRatio = width/height
+
+        if AspectRatio == 16/10:
+            blank1 = Frame(self.rightFrame, width=50).grid(column=0, row=0, rowspan=30, sticky=(N,E,S,W))
+            blank2 = Frame(self.rightFrame, width=50).grid(column=5, row=0, rowspan=30, sticky=(N,E,S,W))
+        elif AspectRatio == 16/9:
+            blank1 = Frame(self.rightFrame, width=190).grid(column=0, row=0,rowspan=30,sticky=(N, E, S, W))
+            blank2 = Frame(self.rightFrame, width=190).grid(column=5, row=0, rowspan=30,sticky=(N, E, S, W))
+        elif AspectRatio == 4/3:
+            blank1 = Frame(self.rightFrame, width=160).grid(column=0, row=0,rowspan=30,sticky=(N, E, S, W))
+            blank2 = Frame(self.rightFrame, width=160).grid(column=5, row=0, rowspan=30,sticky=(N, E, S, W))
+        elif AspectRatio == 3/2:
             blank1 = Frame(self.rightFrame, width=160).grid(column=0, row=0,rowspan=30,sticky=(N, E, S, W))
             blank2 = Frame(self.rightFrame, width=160).grid(column=5, row=0, rowspan=30,sticky=(N, E, S, W))
         else:
             blank1 = Frame(self.rightFrame, width=50).grid(column=0, row=0, rowspan=30, sticky=(N, E, S, W))
             blank2 = Frame(self.rightFrame, width=50).grid(column=5, row=0, rowspan=30, sticky=(N, E, S, W))
-        '''
+
         self.rightFrame.update()
         blank1 = Frame(self.rightFrame, width=self.rightFrame.winfo_width() * .08).grid(column=0, row=0, rowspan=30, sticky=(N, E, S, W))
         blank2 = Frame(self.rightFrame, width=self.rightFrame.winfo_width() * .08).grid(column=5, row=0, rowspan=30, sticky=(N, E, S, W))
