43d14f83e0b978566722480541a9c4c142792636
BrianReddersen <reddersenbrian@gmail.com>
2021-03-19 00:11:42 -0400

    Add files via upload
    updated to match the new format since i somehow managed to break docparser

--- jsonAppend.py
+++ jsonAppend.py
@@ -1,30 +1,41 @@
 from json import *
 
-targetFile = 'muscInst.json'
+targetFile = 'nursing.json'
 
 def wr(data, fname = targetFile):
     with open(fname, 'w') as f:
         dump(data, f, indent = 4)
 
-def append(st):
+def append(st,x):
     with open(targetFile) as file:
-        d=load(file)
-        t = d['four_year']
+        d = load(file)
+        t = d['semester_{}'.format(x)]
         arr = st.split(',')
         y = {
-            "semester": "Year {0} Semester {1}".format(arr[0], arr[1]),
-            "subject": "{0}".format(arr[2]),
-            "catalog": "{0}".format(arr[3]),
-            "title": "{0}".format(arr[4]),
-            "cred": int("{0}".format(arr[5]))
+            "subject": "{0}".format(arr[0].upper()),
+            "catalog": "{0}".format(arr[1]),
+            "title": "{0}".format(arr[2]),
+            "cred": int("{0}".format(arr[3]))
         }
         t.append(y)
     wr(d)
 
 
-print("enter year, semester number, subject, catalog num, title, and credit hours separated by 1 comma")
-print("ex 1,1,ENGL,103,Composition and research, 4")
+print("enter subject, catalog num, title, and credit hours separated by 1 comma")
+print("ENGL,103,Composition and research,4")
+print("enter \'next\' to go to the next semester, \'prev\' goes back 1")
+x = 1
+print("editing semester 1")
 while True:
     i = input(">")
-    append(i)
-
+    if (i == "done"):
+        exit()
+    elif (i == "next"):
+        x += 1
+        print("editing semester {}".format(x))
+        continue
+    elif (i == "prev"):
+        x -= 1
+        continue
+    else:
+        append(i, x)
