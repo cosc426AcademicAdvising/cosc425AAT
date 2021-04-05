d49f211ca9f0f73a95906c071a313ec2feaa2421
BrianReddersen <reddersenbrian@gmail.com>
2021-04-05 16:16:22 -0400

    Update Model.py

--- Model.py
+++ Model.py
@@ -1,10 +1,13 @@
 from tkinter.filedialog import askopenfilename
 import json
-from pubsub import pub      # pip install PyPubSub
+from pubsub import pub  # pip install PyPubSub
 import pymongo
+from bson.regex import Regex
 
-client = pymongo.MongoClient("mongodb+srv://COSC425AAT:ucciEcY4ItzL6BRN@cluster0.qmhln.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+client = pymongo.MongoClient(
+    "mongodb+srv://COSC425AAT:ucciEcY4ItzL6BRN@cluster0.qmhln.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
 db = client['COSC425AAT']
+
 
 class Model:
     def __init__(self):
@@ -25,7 +28,7 @@ class Model:
         for i in obj:
             minors.append(i['Acad Plan'])
         return minors
-    
+
     def getSchools(self):
         myCol = db.get_collection("Department")
         schools = myCol.distinct('School')
@@ -61,7 +64,7 @@ class Model:
         return courseInfo
 
     # Displays what prereqs are necessary for a subject + catalog
-    def getPreReq(subject, catalog):
+    def getPreReq(self, subject, catalog):
         myCol = db.get_collection('Course')
         obj = myCol.find_one({'$and': [{'Subject': subject}, {'Catalog': catalog}]})
         print(obj['RQ Descr(Descrlong)'])
@@ -71,191 +74,193 @@ class Model:
         obj = myCol.distinct('Subject')
         return obj
 
+    def pullStud(self, id, fname):
+        client = pymongo.MongoClient(
+                "mongodb+srv://COSC425AAT:ucciEcY4ItzL6BRN@cluster0.qmhln.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+        db = client['COSC425AAT']
+        stud = db["Student"]
+        query = {"s_id": id}
+        curs = stud.find(query)
+        data = {}
+        for i in curs:
+            data["name"] = i['name']
+            data["s_id"] = i['s_id']
+            data["major"] = i['major']
+            data["dept"] = i['dept']
+            data['minor'] = i['minor']
+            data["status"] = i['status']
+            data["year"] = i['year']
+            data["credits"] = i['credits']
+            data["sem_id"] = i['sem_id']
+            data["registering_for"] = i['registering_for']
+            data["enrll"] = i['enrll']
+            data["advisor_mail"] = i['advisor_mail']
+            data["memo"] = i['memo']
+            data["course_taken"] = i['course_taken']
+            data["taking_course"] = i['taking_course']
+            data["backup_course"] = i['backup_course']
+            data["four_year"] = i['four_year']
+            with open(fname, 'w+') as f:
+                json.dump(data, f, indent=4)
+
     def getStudent(self, sname, sid):
         myCol = db.get_collection('Student')
-        #obj2 = myCol.aggregate([{u"$project": {u"count": {u"$size": u"$course_taken"}}}])
-        #for i in obj2:
-        #    cnt = int(i['count'])
+        obj2 = myCol.aggregate([{u"$project": {u"count": {u"$size": u"$course_taken"}}}])
+        for i in obj2:
+            cnt = int(i['count'])
         obj = myCol.find_one({'$and': [{'name': str(sname)}, {'s_id': int(sid)}]})
-        numbCourses = 0
+        numbCourses = cnt
         cred = 0
         courses = []
         backup = []
-        fourList = []
         for c in obj['taking_course']:
             courseID = c['subject'] + " " + c['catalog']
             courses.append((courseID, c['title'], c['cred'], c['genED']))
             cred += c['cred']
 
         for c in obj['backup_course']:
-            courseID = c['subject']+ " " +c['catalog']
+            courseID = c['subject'] + " " + c['catalog']
             backup.append((courseID, c['title'], c['cred'], c['genED']))
 
-        courseHist = []  # four year plan list (return value)
-        sem = "1"  # Keeps track of which semester in database
-        total = 0  # Total number of semesters
-        ctotal = 0  # Total number of courses in a semester
-
-        myCol = db.get_collection('Student')
-
-        # Gets total number of semesters through error handling
-        for j in range(15):  # Max of 15 possible semesters taken
-            stri = "semester_"  # Append which semester to string
-            stri = stri + sem
-            try:  # Error checks is semester is out of range
-                (obj['course_taken'][0][stri])  # Sets the total to the currently viewed semester
-                total = int(sem)
-            except KeyError as b:
-                total = total  # Last none KeyError semester is stored
-            sem = str(int(sem) + 1)
-        # print(total)
-
-        for k in range(total):  # Iterates through each semester from previously calculated value
-            stri = "semester_"  # Appends which semester to a string
-            stri = stri + str(k + 1)
-            # Gets total number of courses through error handling
-            courseList = []
-            for l in range(8):  # Max of 8 possible courses taken during any given semester
-
-                try:  # Checks for Array index error
-                    (obj['course_taken'][0][stri][l])
-                    ctotal = l + 1  # Sets total number of courses to currently viewed course
-                    resl = [k, obj['course_taken'][0][stri][l]['subject'], obj['course_taken'][0][stri][l]['catalog'],
-                            obj['course_taken'][0][stri][l]['title'],
-                            obj['course_taken'][0][stri][l]['credits'], obj['course_taken'][0][stri][l]['grade']]  # Creates a string value of each objects within array
-                    courseList.append(resl)  # Appends that string to a course list
-                    numbCourses = numbCourses + 1
-                except IndexError as d:
-                    ctotal = ctotal  # Last none index error course number is stored
-            # print(ctotal)
-            courseHist.append(courseList)
-
-        fourList = self.getFourYear(obj['major'])
-
-        pub.sendMessage("PPW_information", obj=obj, tcred=cred, courses=courses, numbCourse=numbCourses, bcourses=backup, courseHist=courseHist, fourYear=fourList, policies=self.getPolicies(obj['major']))
-
-    def updateStudent(self, obj):
-        myCol = db.get_collection('Student')
-        stud = myCol.find_one({'s_id': int(obj['s_id'])})
-
-        # Original total number of course student plans to take
-        CTtotal = len(stud['taking_course'])
-
-        # Original total number of courses student has for backups
-        BUtotal = len(stud['backup_course'])
-
-        # Iterate through each course a student plans to take and update the fields in the database
-        for i in range(len(obj['taking_course'])):
-            subcat = obj['taking_course'][i][0].split()
-            field1 = "taking_course." + str(i) + ".subject"
-            field2 = "taking_course." + str(i) + ".catalog"
-            field3 = "taking_course." + str(i) + ".title"
-            field4 = "taking_course." + str(i) + ".cred"
-            field5 = "taking_course." + str(i) + ".genED"
-            myCol.update_one({'s_id': int(obj['s_id'])},
-                             {'$set': {
-                                 field1: subcat[0],
-                                 field2: subcat[1],
-                                 field3: obj['taking_course'][i][1],
-                                 field4: obj['taking_course'][i][2],
-                                 field5: obj['taking_course'][i][3]
-                             }}
-                             )
-
-        # Iterate through remaining courses and assign null value to subject indicating their need for removal
-        for i in range(len(obj['taking_course']), CTtotal):
-            field1 = "taking_course." + str(i) + ".subject"
-            myCol.update_one({'s_id': int(obj['s_id'])},
-                             {'$set': {
-                                 field1: 'null'
-                             }})
-
-        # Iterate again through remaining courses and pull those courses from database that are no longer needed
-        for i in range(len(obj['taking_course']), CTtotal):
-            myCol.update_one({'s_id': int(obj['s_id'])},
-                             {'$pull': {
-                                 'taking_course': {'subject': 'null'}
-                             }})
-
-        # Iterate through each backup course and update the fields in the database
-        for i in range(len(obj['backup_course'])):
-            subcat = obj['backup_course'][i][0].split()
-            field1 = "backup_course." + str(i) + ".subject"
-            field2 = "backup_course." + str(i) + ".catalog"
-            field3 = "backup_course." + str(i) + ".title"
-            field4 = "backup_course." + str(i) + ".cred"
-            field5 = "backup_course." + str(i) + ".genED"
-            myCol.update_one({'s_id': int(obj['s_id'])},
-                             {'$set': {
-                                 field1: subcat[0],
-                                 field2: subcat[1],
-                                 field3: obj['backup_course'][i][1],
-                                 field4: obj['backup_course'][i][2],
-                                 field5: obj['backup_course'][i][3]
-                             }}
-                             )
-
-        # Iterate through remaining courses and assign null value to subject indicating their need for removal
-        for i in range(len(obj['backup_course']), BUtotal):
-            field1 = "backup_course." + str(i) + ".subject"
-            myCol.update_one({'s_id': int(obj['s_id'])},
-                             {'$set': {
-                                 field1: 'null'
-                             }})
-
-        # Iterate again through remaining courses and pull those courses from database that are no longer needed
-        for i in range(len(obj['backup_course']), BUtotal):
-            myCol.update_one({'s_id': int(obj['s_id'])},
-                             {'$pull': {
-                                 'backup_course': {'subject': 'null'}
-                             }})
-
-    def getPolicies(self, major):
-        myCol = db.get_collection('FourYear')
-        i = myCol.find_one({'major': major})
-        return i['policies']
-
-    def getFourYear(self, major):
         courseList = []  # course list
         fourList = []  # four year plan list (return value)
         sem = "1"  # Keeps track of which semester in database
         total = 0  # Total number of semesters
         ctotal = 0  # Total number of courses in a semester
 
+        for i in obj['four_year']:
+
+            # Gets total number of semesters through error handling
+            for j in range(15):  # Max of 15 possible semesters taken
+                stri = "semester_"  # Append which semester to string
+                stri = stri + sem
+                try:  # Error checks is semester is out of range
+                    (i[stri])  # Sets the total to the currently viewed semester
+                    total = int(sem)
+                except KeyError as b:
+                    total = total  # Last none KeyError semester is stored
+                sem = str(int(sem) + 1)
+            # print(total)
+
+            for k in range(total):  # Iterates through each semester from previously calculated value
+                stri = "semester_"  # Appends which semester to a string
+                stri = stri + str(k + 1)
+                # Gets total number of courses through error handling
+                courseList = []
+                for l in range(8):  # Max of 8 possible courses taken during any given semester
+
+                    try:  # Checks for Array index error
+                        (obj['four_year'][0][stri][l])
+                        ctotal = l + 1  # Sets total number of courses to currently viewed course
+                        resl = [k, i[stri][l]['subject'], i[stri][l]['catalog'], i[stri][l]['title'],
+                                i[stri][l]['credits']]  # Creates a string value of each objects within array
+                        courseList.append(resl)  # Appends that string to a course list
+                    except IndexError as c:
+                        ctotal = ctotal  # Last none index error course number is stored
+                # print(ctotal)
+                fourList.append(courseList)  # Appends the course list to the four year plan list
+
+            # First array initializer corresponds to which semester you are viewing course for
+            # Ex.  fourList[0][1]  =  The first semester and the second course the took that semester
+
+            # All below represnt the second array initializer which corresponds to individual information for a course
+            # [0] = The first value indicates which semester the course is for
+            # [1] = The subject of the course
+            # [2] = The catalog of the course
+            # [3] = The title of the course
+            # [4] = The number of credits for the course
+
+            # [0, 'ENGL', '103', 'Composition and Research', '4']    Example output for fourList[0][2]
+
+        pub.sendMessage("PPW_information", obj=obj, tcred=cred, courses=courses, numbCourse=numbCourses,
+                        bcourses=backup, courseHist=fourList)
+        # pub.sendMessage("FYP_information", obj=obj, courseHist=fourList)
+
+    def delStud(self, id):
+        stud = db["Student"]
+        query = {"s_id": int(id)}
+        info = stud.delete_many(query)
+        if info.deleted_count == 1:
+            return "one entry deleted"
+        elif info.deleted_count == 0:
+            return "no matches found, deleted 0 entries"
+        else:
+            return str(info.deleted_count) + " entries deleted"
+
+    def delCrs(self, sub, num):
+        crs = db["Course"]
+        query = {"Subject": sub, "Catalog": Regex(u".*{0}.*".format(num), "i")}
+        # this ".*string.*" regex searches for entries containing the given string
+        # when searching for a given catalog num normally, the entire string must match
+        # a portion of the entries in the db have spaces at the beginning of the catalog num string
+        # this regex ignores that space and is true if the field contains the given number anywhere in the string
+        info = crs.delete_many(query)
+        if info.deleted_count == 1:
+            return "one entry deleted"
+        elif info.deleted_count == 0:
+            return "no matches found, deleted 0 entries"
+        else:
+            return str(info.deleted_count) + " entries deleted"
+
+    def delDept(self, acad):
+        dept = db["Department"]
+        query = {"Subject": acad}
+        info = dept.delete_many(query)
+        if info.deleted_count == 1:
+            return "one entry deleted"
+        elif info.deleted_count == 0:
+            return "no matches found, deleted 0 entries"
+        else:
+            return str(info.deleted_count) + " entries deleted"
+
+    def getFourYearLayout(sname, sid):
         myCol = db.get_collection('FourYear')
-        i = myCol.find_one({'major': major})
+        crsList = []
+        obj = myCol.distinct('four_year.semester', {'id': sid})
+        for i in obj:
+            crsList.append(i)
+        print(crsList)
 
-        #fourList.append(i['policies'])
+        fourList = []
+        # For loop to iterate through all entries within the distinct list for comparison
+        # Length of course list is number of distinct semesters for a certain student
+        for x in range(len(crsList)):
+            # aggregate pipeline
+            pipe = myCol.aggregate([
+                {
+                    # unwind array for individual comparisons
+                    '$unwind': '$four_year'
+                },
+                {
+                    # match the semester with the distinct semester list
+                    '$match': {'four_year.semester': crsList[x],
+                               'id': sid,
+                               'name': sname}
+                },
+                {
+                    # group all values to a new object and get a count for total number of courses within
+                    '$group': {'_id': '$four_year.semester',
+                               'sub': {'$push': '$four_year.subject'},
+                               'cat': {'$push': '$four_year.catalog'},
+                               'tit': {'$push': '$four_year.title'},
+                               'crd': {'$push': '$four_year.cred'},
+                               # Number of courses within the semester
+                               'count': {'$sum': 1}
+                               }
+                },
 
-        # Gets total number of semesters through error handling
-        for j in range(15):  # Max of 15 possible semesters taken
-            stri = "semester_"  # Append which semester to string
-            stri = stri + sem
-            try:  # Error checks is semester is out of range
-                (i[stri])  # Sets the total to the currently viewed semester
-                total = int(sem)
-            except KeyError as b:
-                total = total  # Last none KeyError semester is stored
-            sem = str(int(sem) + 1)
-        # print(total)
+            ])
+            # Outputs all objects returned by pipeline aggregate and appends them to a list
+            # Through a double for loop iterating through the object and number of courses
+            for i in pipe:
+                val = i['count']
+                for j in range(val):
+                    str = [i['_id'], i['sub'][j], i['cat'][j], i['tit'][j], i['crd'][j]]
+                    fourList.append(str)
 
-        for k in range(total):  # Iterates through each semester from previously calculated value
-            stri = "semester_"  # Appends which semester to a string
-            stri = stri + str(k + 1)
-            # Gets total number of courses through error handling
-            courseList = []
-            for l in range(8):  # Max of 8 possible courses taken during any given semester
+        # How to access the elements within the list
+        # 0 - Semester  1 - Subject  2 - Catalog  3 - Title  4 - Credits
 
-                try:  # Checks for Array index error
-                    (i[stri][l])
-                    ctotal = l + 1  # Sets total number of courses to currently viewed course
-                    resl = [k, i[stri][l]['subject'], i[stri][l]['catalog'], i[stri][l]['title'],
-                            i[stri][l]['credits']]  # Creates a string value of each objects within array
-                    courseList.append(resl)  # Appends that string to a course list
-                except IndexError as c:
-                    ctotal = ctotal  # Last none index error course number is stored
-            # print(ctotal)
-            fourList.append(courseList)
         return fourList
 
     def openJson(self):
